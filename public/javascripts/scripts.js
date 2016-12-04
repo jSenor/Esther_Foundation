c73ec0a8ec2b6a269b081a7a681705fc59acf4ed
Wisdom <wizzy232@gmail.com>
2016-12-04 08:37:13 +0100

    Completed initial design for the header

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,5 +1,33 @@
 window.onload = function(){
-	setInterval(reload, 1000);
+	// setInterval(reload, 1000);
+
+	//Set the height position of the jumbotron text
+	//placeJumboText();
+
+	/*window.onresize = function(){
+		placeJumboText();
+	}*/
+
+	toggleNavigation();
+}
+
+function placeJumboText(){
+	var jumboText = document.querySelector(".jumbo-text");
+	var jumbo = document.querySelector(".jumbotron img");
+	var jumboHeight = Number(jumbo.height);
+	jumboText.style.top = (jumboHeight / 2) + "px";
+}
+
+function toggleNavigation(){
+	var navicon = document.querySelector(".nav-toggle");
+	navicon.addEventListener("click", function(event){
+		var currentClassName = event.target.parentElement.className;
+		if(currentClassName == "navigation inactive"){
+			event.target.parentElement.className = "navigation active";
+			return;
+		}
+		event.target.parentElement.className = "navigation inactive";
+	});
 }
 
 function reload(){
